315c04c5d4d3146684eff2870cda92e55b437a19
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2023-07-28 14:47:56 +0200

    🚸 Indicateur de chargement de la navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {BehaviorSubject, Observable, tap} from "rxjs";
+import {NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
 
 
 @Component({
@@ -6,5 +8,33 @@ import {Component} from "@angular/core";
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+    _navigationLoading$!: BehaviorSubject<boolean>;
+
+
+    constructor(
+        private router: Router,
+    ) {
+        this._navigationLoading$ = new BehaviorSubject<boolean>(false);
+    }
+
+
+    ngOnInit(): void {
+        this.router.events.pipe(
+            tap(event => {
+                if (event instanceof NavigationStart) {
+                    this._navigationLoading$.next(true);
+                } else if (event instanceof NavigationEnd || event instanceof NavigationCancel || event instanceof NavigationError) {
+                    this._navigationLoading$.next(false);
+                }
+            })
+        ).subscribe();
+    }
+
+
+    get navigationLoading$(): Observable<boolean> {
+        return this._navigationLoading$.asObservable();
+    }
+
 }
